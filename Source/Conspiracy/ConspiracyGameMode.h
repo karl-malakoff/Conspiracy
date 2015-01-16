bc59f2e971555bf556a95c4a596809556992d386
Matthew Wood <mattwood2855@gmail.com>
2015-01-16 17:28:06 -0500

    Initial Commit
    Basic Unreal 4.6.1 Project

---
+++ Source/Conspiracy/ConspiracyGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ConspiracyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONSPIRACY_API AConspiracyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
